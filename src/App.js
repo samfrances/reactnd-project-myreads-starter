4ff091e32d2cbec737a10432eedf6c8e082909f1
Sam Frances <sam@samfrances.uk>
2017-08-08 00:42:09 +0100

    Add API request to get books from server

--- src/App.js
+++ src/App.js
@@ -7,13 +7,30 @@ import { Route } from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
+    books: []
+  }
 
+  async componentDidMount() {
+    const booksData = await BooksAPI.getAll();
+    const books = booksData.map(
+      ({ title, industryIdentifiers, authors, imageLinks, shelf }) =>
+        ({
+          title,
+          shelf,
+          id: industryIdentifiers[0].identifier,
+          author: authors[0],
+          coverURL: imageLinks.smallThumbnail,
+        })
+    )
+    this.setState({ books })
   }
 
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={BookList} />
+        <Route exact path="/" render={() => (
+          <BookList books={this.state.books} />
+        )} />
         <Route path="/search" component={SearchPage} />
       </div>
     )
