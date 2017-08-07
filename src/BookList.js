c7dd3a352d079ad05b7c67737b232b7d17c38981
Sam Frances <sam@samfrances.uk>
2017-08-07 21:55:07 +0100

    Add routing with react-router-dom

--- src/BookList.js
+++ src/BookList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BookList = (props) =>
   <div className="list-books">
@@ -157,7 +158,7 @@ const BookList = (props) =>
       </div>
     </div>
     <div className="open-search">
-      <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+      <Link to="/search">Add a book</Link>
     </div>
   </div>;
 
