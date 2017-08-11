410ab685eb56c7b981a0fcfb4e04320969f47718
Sam Frances <sam@samfrances.uk>
2017-08-12 00:48:01 +0100

    Eslint BookDetail.js

--- src/BookDetail.js
+++ src/BookDetail.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import _ from 'lodash';
 
 import { titleCase } from './utils';
 
-const BookDetail = ({ book, shelf="none", shelfOptions=[], changeShelf=f=>f }) =>
+const BookDetail = ({ book, shelf = 'none', shelfOptions = [], changeShelf = f=>f }) =>
   <li>
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{
-          width: 128,
-          height: 193,
-          backgroundImage: (
-            book.imageLinks
-              ? `url("${book.imageLinks.thumbnail}")`
-              : null
-          )
-        }}></div>
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: (
+              book.imageLinks
+                ? `url("${book.imageLinks.thumbnail}")`
+                : null
+            ),
+          }}
+        />
         <div className="book-shelf-changer">
           <select
             value={shelf}
-            onChange={(e) => changeShelf(book, e.target.value)}
+            onChange={e => changeShelf(book, e.target.value)}
           >
             <option value="none" disabled>Move to...</option>
             {
-              shelfOptions.map((option) =>
+              shelfOptions.map(option =>
                 <option key={option} value={option}>
                   {titleCase(option)}
                 </option>
